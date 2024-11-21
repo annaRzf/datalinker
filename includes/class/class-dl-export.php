ecb48727d799d989500a888b5f5af4e5c2f11dc1
Anna Razafindramiarina <razafindramiarinaanna@gmail.com>
2024-11-21 10:01:19 +0300

    feat: Enhance post types and taxonomies retrieval with formatting and default options

--- includes/class/class-dl-export.php
+++ includes/class/class-dl-export.php
@@ -6,12 +6,28 @@ class DataLinkeRExport
     use DataLinkeRHelpers;
 
     /**
-     * Retrieve an array of public post types.
+     * Retrieves and formats the available post types.
      *
-     * This function fetches all public post types and converts them into an associative array
-     * where the keys are the post type names and the values are the post type labels.
+     * This function fetches built-in, custom post types with UI, and custom post types without UI.
+     * It then merges these post types, applies a filter, and assigns icons to each post type.
+     * Finally, it adds a default option and returns the formatted array of post types.
      *
-     * @return array An associative array of public post types with post type names as keys and labels as values.
+     * @return array An associative array of post types with their corresponding icons.
+     *               The array structure is:
+     *               [
+     *                   'post_type_key' => [
+     *                       'text' => 'Post Type Object',
+     *                       'icon' => 'FontAwesome Icon Class'
+     *                   ],
+     *                   ...
+     *               ]
+     *               The array also includes a default option:
+     *               [
+     *                   ' ' => [
+     *                       'text' => 'Select a post type'
+     *                   ],
+     *                   ...
+     *               ]
      */
     public function get_post_types()
     {
@@ -40,6 +56,49 @@ class DataLinkeRExport
                 'icon' => $icon
             ];
         }
+        // Add default option
+        $post_types_array = array_merge([' ' => ['text' => 'Select a post type']], $post_types_array);
+
         return $post_types_array;
+    }
+
+    /**
+     * Retrieves and formats a list of taxonomies.
+     *
+     * This function fetches all available taxonomies, filters out the ones that are
+     * ignored, maps them to a specific format, sorts them alphabetically, and adds
+     * a default option at the beginning of the list.
+     *
+     * @return array An array of taxonomies formatted as ['text' => 'Taxonomy Label'].
+     *               The array includes a default option [' ' => ['text' => 'Select a taxonomy']].
+     */
+    public function get_taxonomies()
+    {
+       // Get all taxonomies
+        $taxonomies = get_taxonomies([], 'objects');
+        $ignore = ['nav_menu', 'link_category'];
+
+        // Filter out ignored taxonomies
+        $filtered_taxonomies = array_filter($taxonomies, function($taxonomy) use ($ignore) {
+            return !in_array($taxonomy->name, $ignore);
+        });
+
+        // Map taxonomies to the desired format
+        $taxonomies_array = array_map(function($taxonomy) {
+            $taxonomy_label = !empty($taxonomy->labels->name) && strpos($taxonomy->labels->name, "_") === false
+                ? $taxonomy->labels->name
+                : (empty($taxonomy->labels->singular_name) ? $taxonomy->name : $taxonomy->labels->singular_name);
+            return ['text' => $taxonomy_label];
+        }, $filtered_taxonomies);
+
+        // Sort taxonomies array
+        uasort($taxonomies_array, function($a, $b) {
+            return strcasecmp($a['text'], $b['text']);
+        });
+
+        // Add default option
+        $taxonomies_array = array_merge([' ' => ['text' => 'Select a taxonomy']], $taxonomies_array);
+
+        return $taxonomies_array;
     }
 }
