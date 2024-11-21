43a4ffb8ff256e843a29aa6a6723bf69c442d196
Anna Razafindramiarina <razafindramiarinaanna@gmail.com>
2024-11-21 10:01:37 +0300

    feat: Add taxonomy visibility toggle based on post type selection in dropdown

--- assets/js/dl-general.js
+++ assets/js/dl-general.js
@@ -1,11 +1,12 @@
 let $
 if (typeof $ === 'undefined') {
-    $ = jQuery;
+    $ = jQuery
 }
 const DLGeneral = {
     init: () => {
         // bind element
-        DLGeneral.initDropdown();
+        DLGeneral.initDropdown()
+        DLGeneral.binTaxonomy()
     },
     initDropdown: () => {
         $('.dl-dropdown-select').select2({
@@ -13,17 +14,29 @@ const DLGeneral = {
             allowClear: true,
             templateResult: DLGeneral.formatOption,
             templateSelection: DLGeneral.formatOption
-        });
+        })
     },
     formatOption: (option) => {
         if (!option.id) {
-            return option.text;
+            return option.text
         }
-        const iconClass = $(option.element).data('icon');
+        const iconClass = $(option.element).data('icon')
         if (iconClass) {
-            return $('<span><i class="' + iconClass + '" style="margin-right: 8px;"></i> ' + option.text + '</span>');
+            return $('<span><i class="' + iconClass + '" style="margin-right: 8px"></i> ' + option.text + '</span>')
         }
-        return option.text;
-    }
+        return option.text
+    },
+    binTaxonomy: () => {
+        $('.dl-dropdown-select[name="post_type"]').change(function() {
+            const postType = $(this).val()
+            console.log(postType)
+            const taxonomySelectContainer = $('.dl-dropdown-select[name="taxonomy"]').parents('.form-group')
+            console.log(taxonomySelectContainer)
+            if( postType == 'taxonomies')
+                taxonomySelectContainer.removeClass('hidden')
+            else
+                taxonomySelectContainer.addClass('hidden')
+        })
+    },
 }
-export default DLGeneral;
+export default DLGeneral
